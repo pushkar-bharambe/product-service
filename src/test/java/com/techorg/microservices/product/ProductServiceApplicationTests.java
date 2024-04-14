79854965da584b40653ba0fe2f4c96597a05a23f
Pushkar Bharambe <pushkarb425@gmail.com>
2024-04-15 01:12:26 +0530

    Integration test changes

--- src/test/java/com/techorg/microservices/product/ProductServiceApplicationTests.java
+++ src/test/java/com/techorg/microservices/product/ProductServiceApplicationTests.java
@@ -1,13 +1,54 @@
 package com.techorg.microservices.product;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
+import io.restassured.RestAssured;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setUp(){
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void testCreateProduct() {
+		String requestBody = """
+					{
+						"name": "Dunkin Doughnuts",
+						"description": "Doughnuts from franchise chain",
+						"price": 10
+					}
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("Dunkin Doughnuts"))
+				.body("description", Matchers.equalTo("Doughnuts from franchise chain"))
+				.body("price", Matchers.equalTo(10));
 	}
 
 }
